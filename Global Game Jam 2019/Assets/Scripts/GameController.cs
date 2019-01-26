3bf896870e5a1b7092930ab1979cfb0e3e05f93c
peachpieproductions <benjames.im@gmail.com>
2019-01-26 13:34:03 -0500

    Start

---
+++ Global Game Jam 2019/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] blockPrefabs;
+    public GameObject currentBlock;
+    public Transform placementSprite;
+    public Camera cam;
+    public Vector2 mouseWorldPos;
+
+    private void Start() {
+        currentBlock = blockPrefabs[Random.Range(0, blockPrefabs.Length)];
+        placementSprite.GetComponent<SpriteRenderer>().sprite = currentBlock.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    public void Update() {
+        mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        placementSprite.position = mouseWorldPos;
+        placementSprite.position += Vector3.back * .1f;
+
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(currentBlock, mouseWorldPos, Quaternion.identity);
+            currentBlock = blockPrefabs[Random.Range(0, blockPrefabs.Length)];
+            placementSprite.GetComponent<SpriteRenderer>().sprite = currentBlock.GetComponent<SpriteRenderer>().sprite;
+        }
+        if (Input.GetMouseButton(1)) {
+            cam.transform.position -= new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"), 0) * Time.deltaTime * (7 + cam.orthographicSize);
+        }
+        cam.orthographicSize = Mathf.Max(2,cam.orthographicSize - Input.mouseScrollDelta.y * .5f);
+    }
+
+    
+
+    
+
+
+
+}
