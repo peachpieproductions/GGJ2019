172c5de896c7670b615e1f498e14c56547ff2b5a
Ben-Laptop <benjames.im@gmail.com>
2019-02-05 23:08:59 -0500

    WZ work, parallax and options started

---
+++ Global Game Jam 2019/Assets/Scripts/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    
+    public float offsetMult = 1;
+    public float animateOffsetSpeed;
+    public Transform cam;
+
+    Vector3 startPos;
+    float xOffset;
+    Material mainMat;
+
+    private void Awake() {
+        startPos = transform.position;
+        mainMat = GetComponent<MeshRenderer>().material;
+    }
+
+
+    private void Update() {
+        transform.position = new Vector3(cam.position.x * offsetMult, startPos.y, transform.position.z);
+        if (animateOffsetSpeed != 0) {
+            xOffset += animateOffsetSpeed * Time.deltaTime * .1f;
+            mainMat.SetTextureOffset("_MainTex", new Vector2 (xOffset, 0));
+        }
+    }
+
+
+
+
+
+
+}
